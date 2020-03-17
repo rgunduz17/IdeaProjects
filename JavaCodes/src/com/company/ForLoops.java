41ad4f28316bdf926494707e002a67e406442065
rgunduz17 <58498991+rgunduz17@users.noreply.github.com>
2020-03-17 11:17:21 -0500

    Done

--- JavaCodes/src/com/company/ForLoops.java
+++ JavaCodes/src/com/company/ForLoops.java
@@ -4,12 +4,45 @@ public class ForLoops {
 
     public static void main(String[] args) {
 
+//  repeating steps multiple times
+
+//          for(init; condition; increment/decrement ){
+//
+//          }
+    //statement will continue until becomes FALSE
+
+        for (  int i =10; i>0; i--) {
+
+            System.out.println("Value of i is :" + i);
+        }
+
+        System.out.println("***********************************");
+
+        for (  int i =1; i<=10; i++) {
+
+            System.out.println("Value of i is :" + i);
+        }
+        System.out.println("***********************************");
 
 
+        for (  int i =1; i<=100; i++) {
+
+            System.out.println("Square of " + i +" is : " + (i*i));
+        }
 
 
+        System.out.println("***********************************");
 
+        String str ="Ridvan Gunduz";
+        String reversed = "";
+        for (int i = str.length()-1; i >=0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println(reversed);
 
         
+        System.out.println("***********************************");
+
     }
 }
