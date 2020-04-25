43f5ee85d536a11d46cbdaf10546b99645d45268
rgunduz17 <rgunduz17@gmail.com>
2020-04-25 15:30:33 -0500

    update the files

--- JavaCodes/src/test/Quiz.java
+++ JavaCodes/src/test/Quiz.java
@@ -4,5 +4,11 @@ public class Quiz {
 
     public static void main(String[] args) {
         System.out.println("Quiz");
+
     }
+
+    // 1. command K button
+    // 2. command shift K buttons
+
+
 }
